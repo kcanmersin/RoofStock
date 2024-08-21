d55e7e28ff4434e990e9ba3297dbc6111d5b258f
kcanmersin <k.mersin2020@gtu.edu.tr>
2024-08-21 13:02:15 +0300

    make internal entity visible for tests

--- Core/obj/Debug/net6.0/Core.AssemblyInfo.cs
+++ Core/obj/Debug/net6.0/Core.AssemblyInfo.cs
@@ -17,6 +17,6 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyProductAttribute("Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
-
+[assembly: InternalsVisibleTo("RoofStockTest.AssemblyInfo")]
 // MSBuild WriteCodeFragment sınıfı tarafından oluşturuldu.
 
